c5fafa36e345b2bdbdc555b4196a089affb4cad9
Jirong <outmanrong@hotmail.com>
2016-05-10 21:58:13 +0800

    rebuild the web page

---
+++ euphytose_v2/v1/index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	<?php
+
+	?>		
+	<title>Home</title>	
+	
+	<link rel="stylesheet" href="css/style.css">
+	<script type="text/javascript" src="./js/jquery.min.js"></script>	
+	<script type="text/javascript" src="./js/animation.js"></script>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
+	<script type="text/javascript" src="./js/config.js"></script>
+	<link href="http://vjs.zencdn.net/4.1/video-js.css" rel="stylesheet">
+	<script src="http://vjs.zencdn.net/4.1/video.js"></script>
+	</head>
+	<body>
+		<div id="bg"></div>
+		<div id="bg_hover"></div>
+		<div id="fond2"></div>
+		<div id="fond3"></div>
+		<div id="titleName"></div>
+		<div id="titleName2"></div>
+		<div id="lineBtn">
+			<div class="btn">
+				<div class="btnNames" id="acc">ACCUEIL</div>
+			</div>
+			<div class="btn">
+				<div class="btnNames" id="eup">EUPHYTOSE<div id="spec">®</div></div>
+			</div>
+			<div class="btn">
+				<div class="btnNames" id="str">STRESS</div>
+			</div>
+			<div class="btn">
+				<div class="btnNames" id="esp">ESPACE ZEN</div>
+			</div>
+		</div>
+		
+		<div class="lineBtn2">
+			<ul class="ul1">
+			  <li id="eup_1">Euphytose®</li>
+			  <li id="eup_2">Une formule </br>efficace</li>
+			  <li id="eup_3">Posologie</li>
+			</ul>
+			<ul class="ul2">
+			  <li id="str_1">Le stress </br>au travail</li>
+			  <li id="str_2">Le stress </br>chez les ados</li>
+			  <li id="str_3">Quizz : </br>le stress et vous</li>
+			</ul>		
+			<ul class="ul3">
+			  <li id="zen_1">Nouvelles thérapies </br>antistress</li>
+			  <li id="zen_2">Ma maison : </br>un espace zen</li>
+			  <li id="zen_3">Le nouveau </br>sport zen</li>
+			  <li id="zen_4">Les secrets </br>d'un bon sommeil</li>
+			  <li id="zen_5">Desserts détente </br>mode d'emploi</li>
+			</ul>
+		
+		</div>	
+		
+		<div id="lineDroite">
+			<div class="signImg"></div>
+			<div class="droiteBtn">SERVICES</div>
+			<div class="signImg"></div>
+			<div class="droiteBtn">PLAN DU SITE</div>
+			<div class="signImg"></div>
+			<div class="droiteBtn">LÉGAL</div>
+			
+			
+		</div>
+		
+		
+		<div id="titleName3"></div>
+		<div id="town"></div>
+		<div id="jar_ter"></div>
+		<div id="buisson"></div>
+
+		<div id="arbre"></div>
+		<div id="plane"></div>
+		<div id="zen"></div>
+		<div id="papillon"></div>
+		<div id="stress"></div>
+		<div id="calque"></div>
+		<div id="eclair"></div>
+		<div id="eclair2"></div>
+		<div id="bird"></div>
+		<div id="bird2"></div>
+		<div id="feuilles"></div>
+		
+		
+		<div id="colonneBlan"></div>
+		<div id="colonneEup"></div>
+		<div id="colonneOis"></div>
+		<div id="colonneGauBlan"></div>
+		
+			<div id="footer">
+			<div id="lienBayer">
+				<div id="lienTitle">LIENS BAYER</div>
+				<div class="liens">·Bayer Santé</div>
+				<div class="liens">·Bayer</div>
+			</div>
+			<div id="signImg2"></div>
+			<div id="liensFooter">
+				<div class="lienFooter">ACCUEIL</div>
+				<div class="lienFooterImg">|</div>
+				<div class="lienFooter">EUPHYTOSE<div id="spec2">®</div></div>
+				<div class="lienFooterImg">|</div>
+				<div class="lienFooter">STRESS</div>
+				<div class="lienFooterImg">|</div>
+				<div class="lienFooter">ESPACE ZEN</div>
+			</div>
+			<div class="textFooter">Médicament de phytothérapie traditionnellement utilisé pour réduire les troubles mineurs de l'anxiété et du sommeil chez l'adulte et les enfants de plus de 6 ans.</div>
+            <div class="textFooter">Chez l'enfant, un trouble du sommeil nécessite de consulter votre médecin. Lire attentivement la notice. Demandez conseil à votre pharmacien.</div>
+            <div class="textFooter">Si les symptômes persistent après 10 jours, consultez votre médecin. VISA N&deg;13/04/68171068/GP/001.</div>
+            <div class="textFooter">*150 ans, la science pour une vie meilleure.</div>
+			<div id="textFooter2">©2013 BAYER SANTÉ FAMILIALE S.A.S - Tous droits réservés/ Dernière mise à jour: xx 2013</div>
+			
+			</div>
+		<div id="feuille1"></div>
+		<div id="stress_hover"></div>
+		<div id="btn_stress"></div>
+		<div id="zen_hover"></div>
+		<div id="btn_zen"></div>
+		
+		<div id="papillonR1"></div>
+		<div id="papillonR2"></div>
+		<div id="papillonR3"></div>
+		<div id="papillonJ1"></div>
+		<div id="papillonJ2"></div>
+		<div id="papillonJ3"></div>
+		<div id="papillonRdy"></div>
+		<div id="papillonJdy"></div>
+		
+		<div id="birdDy1"></div>
+		<div id="birdDy2"></div>
+		<div id="birdDy3"></div>
+		<div id="birdDy4"></div>
+		<div id="colonneImage"></div>
+		<div id="colonneStru"></div>
+		
+		<div id="minAiguille"><div id="imgMin"></div></div>
+		<div id="hourAiguille"><div id="imgHour"></div></div>
+		<div id="videoBtn"></div>
+		<div id="horloge"></div>
+		
+		<div class="video" style="position: absolute; z-index: 1001; margin: -180px 300px auto; display: none; height: 440px; background-color:white;">
+				<video id="example_video_1" class="video-js vjs-default-skin" controls preload="auto" width="600" height="400" poster="http://g8c.uranus.cdigital.fr/newsite/images/video1.jpg" data-setup='{"example_option":true}'>
+ 					<source src="http://g8c.uranus.cdigital.fr/newsite/video/video1.mp4" type='video/mp4' />
+ 					<source src="http://g8c.uranus.cdigital.fr/newsite/video/video1.webm" type='video/webm' />
+					<source src="http://g8c.uranus.cdigital.fr/newsite/video/video1.ogg" type='video/ogg' />
+				</video>
+		<div class="tv_info"></div>
+		<div class="tv_closeBtn"></div>
+		</div>
+		
+	</body>
+</html>
+
+
+
